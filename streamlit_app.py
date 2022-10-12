2119c9d6181dac255a91e823f6af50c3bcf912c8
JuliaPython <JuliaPython@users.noreply.github.com>
2022-10-12 16:05:36 +0200

    Update streamlit_app.py
    added user input for fruit search

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,10 @@ streamlit.dataframe(fruits_to_show)
 # new section to show fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # transform json format to readable dataframe
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
